f7bd0f9a2e4a0684469eb5e9008b0f25c0a6e3a4
Shahid Raza <shahidraza02qwert@gmail.com>
2024-01-15 22:46:36 +0530

    modify repository, controllers ,model and router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
 import userRouter from "./src/features/users/user.route.js";
-import { mongooseConnection } from "./src/configures/mongose.config.js";
+import { connectDB } from "./src/configures/mongose.config.js";
 import { authJWT } from './src/middlewares/authJWT.middleware.js';
 import session from 'express-session';
 import passport from 'passport';
@@ -12,21 +12,23 @@ import cors from 'cors';
 // authentication configs
 import { googleAuth } from './src/configures/googleOauth.config.js';
 import {facebookAuth} from "./src/configures/facebookOauth.config.js";
+import friendRouter from './src/features/friends/friends.route.js';
 googleAuth();
 facebookAuth();
 // create server
 const app=express();
 const port =8080;
-// connecting mongooose
-mongooseConnection();
+
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+
 
 // CORS policy configure
 // app.use(cors({
     //     origin:"http://192.168.43.177:3000",
     //     allowedHeaders:"*"
     // }))
-    
-    app.use(express.json({extended:true}));
     // session middleware
     app.use(session({
         secret:"SECRETE",
@@ -42,28 +44,33 @@ mongooseConnection();
     
     // import user routers
     app.use("/api/users",userRouter);
+    app.use('/api/friends',authJWT,friendRouter);
+    
+    // const corsConfig=
+    //     {
+        //         // origin:"https://incredible-twilight-3ebe40.netlify.app/",
+        //         origin:"http://localhost:3000",
+        //         allowedHeaders:"*",
+        //         credentials:true,
+        //         optionsSuccessStatus:200
+        //     }
+        
+        
+        // app.use(cors(corsConfig));
+        
+        //defalut respone and req
+        app.get('/',(req,res)=>{
+            return res.send(" Hii shahid ,how are you");
+        });
 
-const corsConfig=
-    {
-        // origin:"https://incredible-twilight-3ebe40.netlify.app/",
-        origin:"http://localhost:3000",
-        allowedHeaders:"*",
-        credentials:true,
-        optionsSuccessStatus:200
-    }
 
-app.use(cors());
-
-
-//defalut respone and req
-app.get('/',(req,res)=>{
-    return res.send(" Hii shahid ,how are you");
-});
-
- 
-app.listen(port,(err)=>{
-    if(err){
-        console.log("Server is not connecting on port :",port);
-    }
-    console.log(`Sever is successfully connected on port: ${port}`);
-})
+        // connecting mongooose
+        connectDB()
+        .then((connectedDb) => {
+          app.listen(port, () => {
+            console.log(`app listening on port ${port}`);
+            console.log(`connected to DB :: ${connectedDb.name}`);
+          });
+        })
+        .catch((error) => console.log(`${error} did not connect`));
+        
